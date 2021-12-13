d064cf9fa9f8592870791638bdcf99ac80f6334f
Ayoub El Guendouz <ayoub.el_guendouz@edu.devinci.fr>
2021-12-13 14:31:42 +0100

    feat(components): Add navbar

---
+++ components/Navbar.js
@@ -0,0 +1,12 @@
+import react, { useState, useEffect } from 'react';
+import styles from '../styles/Navbar.module.css';
+
+export default function Navbar() {
+  return (
+    <div className={styles.navcontainer}>
+      <p>trst</p>
+      <p>trst</p>
+      <p>trst</p>
+    </div>
+  );
+}
