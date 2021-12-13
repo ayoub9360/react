b61f7cb1b24827e577d4de611f5a51d205693354
Ayoub El Guendouz <ayoub.el_guendouz@edu.devinci.fr>
2021-12-13 14:44:45 +0100

    feat(components): Add navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,11 +2,24 @@ import react, { useState, useEffect } from 'react';
 import styles from '../styles/Navbar.module.css';
 
 export default function Navbar() {
+  const [user, setUser] = useState(null);
   return (
     <div className={styles.navcontainer}>
-      <p>trst</p>
-      <p>trst</p>
-      <p>trst</p>
+      <h3>NFT Marketplace</h3>
+      <p>Menu</p>
+      <div>
+        {
+          user ? ( 
+            <button>
+              Logout
+            </button> 
+          ) : (
+            <button>
+              Login
+            </button>
+          )
+        }
+      </div>
     </div>
   );
 }
